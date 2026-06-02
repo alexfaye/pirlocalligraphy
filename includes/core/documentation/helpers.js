06752526a0c1aa201cadfef218ff9e62ada0b907
alexfaye <pirlow@gmail.com>
2026-06-02 17:47:04 +0930

    int_ga4_transation created and updated

--- includes/core/documentation/helpers.js
+++ includes/core/documentation/helpers.js
@@ -1,5 +1,9 @@
 const ga4Events = require("includes/core/documentation/modules/ga4/ga4_events.json");
+const ga4Sessions = require("includes/core/documentation/modules/ga4/ga4_session.json");
+const ga4Transactions = require("includes/core/documentation/modules/ga4/ga4.transaction.json");
 
 module.exports = {
     ga4Events,
+    ga4Sessions,
+    ga4Transactions,
 };
